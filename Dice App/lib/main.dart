7ffee8831756a141e8210b4e5e86cfb157c95030
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-06 15:50:01 -0800

    added buttons on the dice images

--- Dice App/lib/main.dart
+++ Dice App/lib/main.dart
@@ -31,15 +31,21 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-              // flex property is used here to make this child twice than the other one.
-              // flex: 2,
-              child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice3.png'),
-          )),
+            // flex property is used here to make this child twice than the other one.
+            // flex: 2,
+            child: FlatButton(
+              // we provide this an anonymous function.
+              onPressed: () {
+                print('Touched left button');
+              },
+              child: Image.asset('images/dice3.png'),
+            ),
+          ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Touched right button');
+              },
               child: Image(
                 // to make the image adaptable we use 'Expanded' widget
                 image: AssetImage('images/dice1.png'),
