b4ebf70b1d887e4c79600e6ee9287a6ff8704bec
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-05 18:22:53 -0800

    added ListTile & Divider

--- Mi_Card/mi_card_flutter/lib/main.dart
+++ Mi_Card/mi_card_flutter/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 CircleAvatar(
                   radius: 50,
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
                   'Aarav Chandra',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 50.0,
+                    fontSize: 65.0,
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Sacramento',
                   ),
@@ -37,71 +38,78 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                // Now we have to make the two childs that have phone
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 55.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.phone,
-                        size: 40.0,
-                        semanticLabel: 'It is the Phone Icon',
-                        textDirection: TextDirection.ltr,
-                        color: Colors.teal,
-                      ),
-                      // To separate the number from icon, we add a SizedBox:
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '+44 (123) 456 789',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
+                SizedBox(
+                  width: 200.0,
+                  height: 10.0,
+                  // Divider - adds the horizontal bar below the 'AWESOME...' text.
+                  child: Divider(
+                    height: 20.0,
+                    thickness: 1.2,
+                    color: Colors.teal.shade100,
                   ),
                 ),
-                Container(
+                // Now we have to make the two childs that have phone using a card class.
+                // Card widget doesn't have padding hence we have to use padding widget enclosing it.
+                Card(
                   color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
                   margin: EdgeInsets.symmetric(
                     vertical: 10.0,
                     horizontal: 55.0,
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.email,
-                        size: 40.0,
-                        semanticLabel: 'It is the Email Icon',
-                        textDirection: TextDirection.ltr,
-                        color: Colors.teal,
-                      ),
-                      // To separate the number from icon, we add a SizedBox:
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'flutter@dev.com',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20,
+                  child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          size: 40.0,
+                          semanticLabel: 'It is the Phone Icon',
+                          textDirection: TextDirection.ltr,
+                          color: Colors.teal,
+                        ),
+                        // To separate the number from icon, we add a SizedBox:
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '+44 (123) 456 789',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // Cards by default comes with white color.
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 55.0,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          size: 40.0,
+                          semanticLabel: 'It is the Email Icon',
+                          textDirection: TextDirection.ltr,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'flutter@dev.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20,
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                )
+                    ))
               ],
             ),
           ),
@@ -110,3 +118,15 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+
+//                     children: [
+
+//                       // To separate the number from icon, we add a SizedBox:
+//                       SizedBox(
+//                         width: 10,
+//                       ),
+
+//                     ],
+//                   ),
