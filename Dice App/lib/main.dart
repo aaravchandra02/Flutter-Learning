828d7641171c5f50987df2cdbfc47ce6477d6ea2
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-06 15:29:54 -0800

    added the design of the app

--- Dice App/lib/main.dart
+++ Dice App/lib/main.dart
@@ -6,7 +6,16 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          centerTitle: true,
+          title: Text(
+            'Dice',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Sacramento',
+            ),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +27,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              // flex property is used here to make this child twice than the other one.
+              // flex: 2,
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice3.png'),
+          )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image(
+                // to make the image adaptable we use 'Expanded' widget
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
