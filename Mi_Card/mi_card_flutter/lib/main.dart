2a045611b8111c8215e5c8cd1d3b13871796cb96
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-05 16:19:32 -0800

    added the phone and email containers

--- Mi_Card/mi_card_flutter/lib/main.dart
+++ Mi_Card/mi_card_flutter/lib/main.dart
@@ -10,33 +10,100 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/diamond.png'),
-              ),
-              Text(
-                'Aarav Chandra',
-                style: TextStyle(
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/diamond.png'),
+                ),
+                Text(
+                  'Aarav Chandra',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Sacramento',
+                  ),
+                ),
+                Text(
+                  'AWESOME DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                // Now we have to make the two childs that have phone
+                Container(
                   color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Sacramento',
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 55.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 40.0,
+                        semanticLabel: 'It is the Phone Icon',
+                        textDirection: TextDirection.ltr,
+                        color: Colors.teal,
+                      ),
+                      // To separate the number from icon, we add a SizedBox:
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '+44 (123) 456 789',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Text(
-                'AWESOME DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 55.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.email,
+                        size: 40.0,
+                        semanticLabel: 'It is the Email Icon',
+                        textDirection: TextDirection.ltr,
+                        color: Colors.teal,
+                      ),
+                      // To separate the number from icon, we add a SizedBox:
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'flutter@dev.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
