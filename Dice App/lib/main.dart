13701d17b50efd98ce687b27000bbb6a76e6d324
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-07 00:23:14 -0800

    converted to stateful widget and added the buttons and ui changes are mapped

--- Dice App/lib/main.dart
+++ Dice App/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -24,35 +25,91 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+// This is the 'state' part of a stateful widget
+class _DicePageState extends State<DicePage> {
+  final random = new Random();
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
+    // Randommly generating the dice numbers.
+
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            // flex property is used here to make this child twice than the other one.
-            // flex: 2,
-            child: FlatButton(
-              // we provide this an anonymous function.
-              onPressed: () {
-                print('Touched left button');
-              },
-              child: Image.asset('images/dice3.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              // it expects an "onPressed" whichmust have void callback function.
-              onPressed: () {
-                print('Touched right button');
-              },
-              child: Image(
-                // to make the image adaptable we use 'Expanded' widget
-                image: AssetImage('images/dice1.png'),
+          Row(
+            children: [
+              Expanded(
+                // flex property is used here to make this child twice than the other one.
+                // flex: 2,
+                child: FlatButton(
+                  // we provide this an anonymous function.
+                  onPressed: () {
+                    // now we need to call the setState() - it tells that the state has been changed
+                    setState(() {
+                      print('Touched left button');
+                      leftDiceNumber = random.nextInt(6) + 1;
+                      print('$leftDiceNumber');
+                    });
+                  },
+                  // This $ is string interpolation
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
               ),
-            ),
-          )
+              Expanded(
+                child: FlatButton(
+                  // it expects an "onPressed" which must have void callback function.
+                  onPressed: () {
+                    setState(() {
+                      print('Touched right button');
+                      rightDiceNumber = random.nextInt(6) + 1;
+                    });
+                  },
+                  child: Image(
+                    // to make the image adaptable we use 'Expanded' widget
+                    image: AssetImage('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 200.0,
+            height: 50.0,
+          ),
+          Column(
+            children: [
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(70.0),
+                ),
+                color: Colors.white,
+                elevation: 30.0,
+                padding: EdgeInsets.all(25.0),
+                child: Text(
+                  'Roll both dices',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    print('rolling both dices');
+                    leftDiceNumber = random.nextInt(6) + 1;
+                    rightDiceNumber = random.nextInt(6) + 1;
+                  });
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
