baee7703a95524e47779b97e9206355c3b4dc0d6
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-08 19:13:08 -0800

    completed code

--- Magic pool/lib/main.dart
+++ Magic pool/lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Magic 8 Ball Decision App'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: MagicApp(),
+        ),
       ),
     );
+
+class MagicApp extends StatefulWidget {
+  @override
+  _MagicAppState createState() => _MagicAppState();
+}
+
+class _MagicAppState extends State<MagicApp> {
+  int number = Random().nextInt(5) + 1;
+  int ran() {
+    number = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      // width: ,
+      child: Column(
+        children: [
+          Expanded(
+            // To make an image as a button we first use a FlatButton and give it an image as a child.
+            child: FlatButton(
+              onPressed: () {
+                // the changes present within are changed
+                setState(() {
+                  print('Yes Pressed');
+                  ran();
+                });
+              },
+              child: Image.asset(
+                'images/ball$number.png',
+                alignment: Alignment.center,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
