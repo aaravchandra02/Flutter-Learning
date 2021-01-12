5738b8d27fa822d91923cb165b817e3d13c62c44
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-12 01:13:37 -0800

    added questions changing funtionality

--- Beginner/Quizzler App/lib/main.dart
+++ Beginner/Quizzler App/lib/main.dart
@@ -26,6 +26,12 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -68,6 +74,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
                 setState(() {
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                  questionNumber++;
                 });
               },
             ),
@@ -92,6 +99,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
                 setState(() {
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  questionNumber++;
                 });
               },
             ),
