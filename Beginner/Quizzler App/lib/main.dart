dcd6d4c5f6093983c90a501d6d300aaef38a3b9d
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-21 16:45:20 -0800

    added abstraction

--- Beginner/Quizzler App/lib/main.dart
+++ Beginner/Quizzler App/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'quizbrain.dart';
 
+QuizBrain quizBrain = QuizBrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -29,13 +30,6 @@ class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
   int totalScore = 0;
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -49,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,12 +71,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                if (questionBank[questionNumber].questionAnswer == true) {
+                if (quizBrain.questionBank[questionNumber].questionAnswer ==
+                    true) {
                   print('The Answer is correct');
                   totalScore++;
                 } else {
                   print(
-                      'The User answered Question $questionNumber Incorrectly\n');
+                      'The User answered Question $questionNumber+1 Incorrectly\n');
                 }
                 setState(() {
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
@@ -109,7 +104,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                if (questionBank[questionNumber].questionAnswer == false) {
+                if (quizBrain.questionBank[questionNumber].questionAnswer ==
+                    false) {
                   print('The Answer is correct');
                   totalScore++;
                 } else {
--- Beginner/Quizzler App/lib/question.dart
+++ Beginner/Quizzler App/lib/question.dart
@@ -1,9 +1,10 @@
 class Question {
+  // these are class properties
   String questionText;
   bool questionAnswer;
 
   // Inorder to give value to our variables we need to create constructor for this class.
-  Question({String q, bool a}) {
+  Question(String q, bool a) {
     questionText = q;
     questionAnswer = a;
   }
